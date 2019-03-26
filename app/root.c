5a6e8277f52f3cc5cc040cd3f709dad4ccac7706
zeropointsix <47769842+zeropointsix@users.noreply.github.com>
2019-03-26 11:53:00 +0300

    Update root.c

--- app/root.c
+++ app/root.c
@@ -2,8 +2,20 @@
 
 
 double* roots(double a, double b, double c){
-    double* root=(double*)malloc(2*sizeof(double));
-    root[0]=(-b+sqrt(b*b-4*a*c))/(2*a);
-    root[1]=(-b-sqrt(b*b-4*a*c))/(2*a);
+    double* root=(double*)malloc(3*sizeof(double));
+    if( (b*b-4*a*c) < 0 ){
+        root[0] = 0;
+    }
+    if( (b*b-4*a*c) == 0 ){
+        root[0] = 1;
+        root[1]=(-b+sqrt(b*b-4*a*c))/(2*a);
+        root[2]=(-b-sqrt(b*b-4*a*c))/(2*a);
+    }
+    
+    if( (b*b-4*a*c) > 0 ){
+        root[0] = 2;
+        root[1]=(-b+sqrt(b*b-4*a*c))/(2*a);
+        root[2]=(-b-sqrt(b*b-4*a*c))/(2*a);
+    }
     return root;
 }
