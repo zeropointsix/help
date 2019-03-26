3bd2306390c5760abef968706f0b69e6b9040c6c
zeropointsix <47769842+zeropointsix@users.noreply.github.com>
2019-03-26 11:54:46 +0300

    Update roottest.h

--- tests/roottest.h
+++ tests/roottest.h
@@ -17,14 +17,16 @@ TEST(rootTest, num0) {
 
 TEST(rootTest, positive) {
     double *a=roots(1, 2, 1);
-    ASSERT_EQ(a[0], -1);
+    ASSERT_EQ(a[0], 1);
     ASSERT_EQ(a[1], -1);
+    ASSERT_EQ(a[2], -1);
 }
 
 TEST(rootTest, negative) {
     double *a=roots(-1, -2, -1);
-    ASSERT_EQ(a[0], -1);
+    ASSERT_EQ(a[0], 1);
     ASSERT_EQ(a[1], -1);
+    ASSERT_EQ(a[2], -1);
 }
 
 #endif // ROOTTEST_H
